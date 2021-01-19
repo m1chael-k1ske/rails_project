1c5f6a3a3e41c700f568e6a89222a0ad70d23ae0
m1chael-k1ske <xxxxxk1skexxxxx@yahoo.co.jp>
2021-01-19 14:36:17 +0000

    Ramsey

---
+++ 1/sffold.rb
@@ -0,0 +1,15 @@
+# Rails scaffold とは
+
+# scaffoldとは、Railsに備わっているコマンドの１つで、ルーティングやコントローラー(controller)、ビュー(view)、モデル(model)と
+# テーブル（データベース）の記述やファイルなどを自動で作成してくれるコマンド、controllerやmodelを作成する手間を省くことができる
+# scaffoldという言葉自体の意味は「土台」 すごく便利
+# scaffoldを使って作成されるルーティングは、Railsが定める７つのアクションになる（index・show・new・edit・create・update・destroyアクションの７つ）
+
+# scaffoldを使い、準備されたテンプレートファイルを利用することで、より迅速なサービス開発ができるようになる
+
+# アプリケーションを作成後、ディレクトリ移動
+
+# rails g scaffold モデル名 カラム名①:データの型① カラム名②:データの型② 
+# rails g scaffold Tweet title:string content:text
+
+# rails db:migrate を忘れないように
