cc43dafbe3f866ec31f15bea80932f911a19f0c8
m1chael-k1ske <xxxxxk1skexxxxx@yahoo.co.jp>
2021-01-21 12:11:23 +0000

    Cuadrado

---
+++ 1/migration_file.rb
@@ -0,0 +1,15 @@
+# Migrationファイルとは？
+  
+# Migrationファイルの状態を戻す
+  
+  rails db:migrateコマンドを実行してテーブルを作成した際
+  記述が間違っていて意図しない名前や型を持つテーブルができてしまった時
+  
+  その際は
+  
+  rails db:rollback
+  
+  コマンドを実行し、直近で実行したMigrationファイルの読み込み前の状態に戻す
+  rails db:migrateコマンドではなく
+  rails db:rollbackを実行する
+  その後、rails db:migrateを実行すると最新のMigrationファイルが再度読み込まれる
